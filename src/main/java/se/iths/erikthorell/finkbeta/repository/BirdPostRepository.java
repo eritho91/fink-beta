ffb1f5c075e157404e397bff7d2b4f9fd0924705
eritho91 <erik.thorell@iths.se>
2026-03-26 09:50:51 +0100

    init commit

--- src/main/java/se/iths/erikthorell/finkbeta/repository/BirdPostRepository.java
+++ src/main/java/se/iths/erikthorell/finkbeta/repository/BirdPostRepository.java
@@ -2,8 +2,9 @@ package se.iths.erikthorell.finkbeta.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import se.iths.erikthorell.finkbeta.model.BirdPost;
 
-@Controller
+@Repository
 public interface BirdPostRepository extends JpaRepository<BirdPost, Long> {
 }
